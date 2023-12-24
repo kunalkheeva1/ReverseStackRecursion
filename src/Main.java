78a890857482af7630268641873a47e962211dfc
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2023-12-23 23:39:09 -0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        st.add(1); st.add(2); st.add(3); st.add(4); st.add(5);
+        stackReverse(st);
+
+        while(!st.isEmpty()){
+            System.out.println(st.pop());
+        }
+    }
+    public static void stackReverse(Stack<Integer> st){
+        if(st.size() == 0) return;
+        int temp = st.pop();
+        stackReverse(st);
+        st.push(temp);
+    }
+}
