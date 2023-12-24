26f11891263d7a58c49565bb8fdd51060e700bec
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2023-12-23 23:45:35 -0800

    sheesh, it didn't work, now lets fix it.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ class Main{
     public static void main(String[] args) {
         Stack<Integer> st = new Stack<>();
         st.add(1); st.add(2); st.add(3); st.add(4); st.add(5);
-        stackReverse(st);
+//        stackReverse(st);
 
         while(!st.isEmpty()){
             System.out.println(st.pop());
