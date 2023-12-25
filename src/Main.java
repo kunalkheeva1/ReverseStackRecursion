1c49697660391771819fddd8a12754ddf8ceda7a
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2023-12-24 21:29:34 -0800

    added insertBottom method, so that when i isolate all the elements, and in between the recursion it goes and add the element at the bottom on the sorted stack,
    till now I know the insertBottom method, but I want to analyze the one by one recursive step of stackReverse as well, I need to work on it.

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,28 @@ class Main{
     public static void main(String[] args) {
         Stack<Integer> st = new Stack<>();
         st.add(1); st.add(2); st.add(3); st.add(4); st.add(5);
-//        stackReverse(st);
+        stackReverse(st);
 
         while(!st.isEmpty()){
             System.out.println(st.pop());
         }
     }
     public static void stackReverse(Stack<Integer> st){
-        if(st.size() == 0) return;
+        if (st.isEmpty()) return;
+
         int temp = st.pop();
         stackReverse(st);
-        st.push(temp);
+        insertBottom(st, temp);
+    }
+    public static void insertBottom(Stack<Integer> st, int x){
+        if(st.isEmpty()){
+            st.push(x);  // once we empty the stack it will insert the desired element
+            return;
+        }
+        // also the recursion continues and add the popped elements
+        int temp = st.pop();
+        insertBottom(st,x);
+        st.push(temp); // after reaching base condition, it will push temp one by one for each recursion
+
     }
 }
